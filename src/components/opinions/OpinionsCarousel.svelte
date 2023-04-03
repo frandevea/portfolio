1cdfcd32c04fa3083bd250ba772bbd53363def57
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-03 05:00:50 +0200

    create opinions carousel component

---
+++ src/components/opinions/OpinionsCarousel.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Marquee from 'svelte-fast-marquee';
+	import OpinionCard from './OpinionCard.svelte';
+	import { opinions } from './opinions.data';
+	import { each } from 'svelte/internal';
+</script>
+
+<Marquee direction="right" speed={10}>
+	{#each opinions as opinion}
+		<OpinionCard {opinion} />
+	{/each}
+</Marquee>
