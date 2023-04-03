930c6c84f1bb19ab813f838e9432611c1eae8061
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-03 05:00:19 +0200

    create a component for the text in the header

---
+++ src/components/WelcomeText.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class="py-16">
+	<div class="flex justify-around items-center flex-wrap">
+		<div class="pr-0 mb-10 lg:pr-5 lg:mb-0">
+			<p class="text-4xl mb-1 font-bold max-w-3xl">Hi! I am Fran Perea.</p>
+			<p class="text-2xl mb-3 font-bold max-w-xl">
+				Transforming <span class="text-indigo-500">ideas</span> into reality through
+				<span class="text-indigo-500">software</span> engineering is my calling.
+			</p>
+		</div>
+		<img src="/src/lib/images/opinions/me.jpg" alt="" class="rounded-full" />
+	</div>
+</div>
