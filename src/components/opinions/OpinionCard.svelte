6b2e819ccdf697a37f7eb699c51d55f547eeb7b7
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-03 05:00:27 +0200

    create an opinion card component

---
+++ src/components/opinions/OpinionCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Opinion } from '../../lib/types/opinion.interface';
+
+	export let opinion: Opinion;
+</script>
+
+<figure class="bg-indigo-50 rounded-xl p-8 md:p-0 dark:bg-slate-800 m-3">
+	<div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+		<blockquote>
+			<p class="text-lg max-w-sm font-medium">
+				“{opinion.description}”
+			</p>
+		</blockquote>
+
+		<figcaption class="font-medium">
+			<div class="flex items-center justify-start">
+				<img class="md:h-auto rounded-full" src={opinion?.picture} alt="" width="100px" />
+				<div class="pl-5">
+					<div class="text-indigo-500">{opinion.name}</div>
+					<div class=" ">
+						{opinion.position}, {opinion.company}
+					</div>
+				</div>
+			</div>
+		</figcaption>
+	</div>
+</figure>
