60968096657a11aebc3403aa6fc1d48ff7ba88e3
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-03 05:00:39 +0200

    add static data for opinions

---
+++ src/components/opinions/opinions.data.ts
@@ -0,0 +1,36 @@
+import type { Opinion } from '../../lib/types/opinion.interface';
+
+export const opinions: Opinion[] = [
+	{
+		name: 'Lucas Goyeche',
+		picture: '/src/lib/images/opinions/lucas.jpg',
+		description:
+			'Capaz de enfrentar cualquier desafío técnico, Fran posee una gran habilidad de diseño acompañada de su abanico de herramientas como desarrollador, combinado con su atención al detalle y comunicación, un placer trabajar con él.',
+		position: 'Tech Lead',
+		company: 'Broken Rubik'
+	},
+	{
+		name: 'Youssef Lehchiki',
+		picture: '/src/lib/images/opinions/youssef.jpg',
+		description:
+			'Recomiendo encarecidamente a Fran como desarrollador freelance. Es altamente competente, confiable y dedicado en su trabajo. Su experiencia y habilidades serán de gran valor para cualquier proyecto en el que se involucre.',
+		position: 'Software Engineer',
+		company: 'Delcom Wave'
+	},
+	{
+		name: 'Manuel Manresa',
+		picture: '/src/lib/images/opinions/manu.jpg',
+		description:
+			'Capaz de enfrentar cualquier desafío técnico, Fran posee una gran habilidad de diseño acompañada de su abanico de herramientas como desarrollador, combinado con su atención al detalle y comunicación, un placer trabajar con él.',
+		position: 'Software Engineer',
+		company: 'Delcom'
+	},
+	{
+		name: 'Luis Enrique',
+		picture: '/src/lib/images/opinions/unknown.jpg',
+		description:
+			'Super competente, contacte con varios programadores que estuvieron a prueba y el que mejor resultado me dió con diferencia fue Fran, todo un acierto.',
+		position: 'CEO',
+		company: 'Viajes Mundomanía'
+	}
+];
