8043818482455bc2b6f3f5d34b2e96adb1f6a171
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-03 05:01:40 +0200

    upload static data for my projects

---
+++ src/components/projects/projects.data.ts
@@ -0,0 +1,20 @@
+import type { Project } from '$lib/types/project.interface';
+
+export const projects: Project[] = [
+	{
+		name: 'Koru Network',
+		image: '/src/lib/images/projects/koru.png',
+		description:
+			'Koru Network, was a stablished minecraft server with more than 100.000 unique players. The server reached up to 40 staff members from all over the globe',
+		link: 'string',
+		linkName: 'linkedin.com'
+	},
+	{
+		name: 'Viajes Mundomanía',
+		image: '/src/lib/images/projects/viajesmundomania.jpg',
+		description:
+			'Viajes Mundomanía is a short-term travel agency within Spain. I developed their current website and automated their booking systems',
+		link: 'string',
+		linkName: 'linkedin.com'
+	}
+];
