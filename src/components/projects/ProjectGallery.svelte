71a475bcb5b8523212cd12ac8a3976f6858e258e
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-04 22:24:41 +0200

    refactor the project gallery

--- src/components/projects/ProjectGallery.svelte
+++ src/components/projects/ProjectGallery.svelte
@@ -1,22 +1,19 @@
 <script>
 	import ProjectCard from './ProjectCard.svelte';
-	import { projects } from './projects.data';
+	import { projects } from '$lib/data/projects.data';
 </script>
 
-<section class="mx-auto max-w-7xl">
-	<div class="p-4 text-left max-w-2xl">
-		<p class="text-4xl mb-1 text-left font-bold pt-10">
-			Things I’ve made trying to put my dent in the universe.
-		</p>
-		<p class="text-small my-5 text-zinc-500">
-			I’ve worked on tons of little projects over the years but these are the ones that I’m most
-			proud of.
-		</p>
+<section class="mx-auto max-w-7xl my-10">
+	<div class="flex items-center justify-center flex-wrap sm:px-4 lg:justify-between">
+		<div class="flex flex-col lg:max-w-xl text-center lg:text-left">
+			<p class="text-4xl mb-1 font-bold">Things I’ve made trying to put my dent in the universe.</p>
+			<p class="text-small my-5 text-zinc-500">
+				I’ve worked on tons of little projects over the years but these are the ones that I’m most
+				proud of.
+			</p>
+		</div>
 	</div>
-	<ul
-		role="list"
-		class="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
-	>
+	<ul class="grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 		{#each projects as project}
 			<ProjectCard {project} />
 		{/each}
