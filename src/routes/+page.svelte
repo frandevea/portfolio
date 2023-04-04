272961d10312a867572f8b47af1891f7a9bd1db3
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-04 22:25:20 +0200

    refactor the structure of the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
 <script>
 	import OpinionsCarousel from '../components/opinions/OpinionsCarousel.svelte';
-	import WelcomeText from '../components/WelcomeText.svelte';
-	import { opinions } from '../components/opinions/opinions.data';
-
-	import OpinionCard from '../components/opinions/OpinionCard.svelte';
 	import ProjectGallery from '../components/projects/ProjectGallery.svelte';
+	import Header from '../components/Header.svelte';
+	import Socials from '../components/Socials.svelte';
+	import TechGallery from '../components/technologies/TechGallery.svelte';
 </script>
 
-<div class="py-10">
+<div class="pt-10">
 	<div class="px-5 md:px-14">
-		<WelcomeText />
+		<Header />
+	</div>
+
+	<div class="px-5 md:px-14">
+		<ProjectGallery />
+		<TechGallery />
 	</div>
 
 	<OpinionsCarousel />
-	<div class="px-5 md:px-14">
-		<ProjectGallery />
-	</div>
 </div>
