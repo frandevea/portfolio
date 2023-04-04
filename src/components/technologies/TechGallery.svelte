4058b7ffc9b1672c143c6522a123a6f98e5410fe
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-04 22:25:06 +0200

    create a section to show the technologies that I have more experience with

---
+++ src/components/technologies/TechGallery.svelte
@@ -0,0 +1,37 @@
+<script>
+	import TechPill from './TechPill.svelte';
+	import { technologies } from '$lib/data/technologies.data';
+</script>
+
+<section class="mt-10 mb-20 flex items-center flex-wrap justify-center lg:justify-between sm:px-4">
+	<div class="flex items-center justify-center flex-wrap lg:justify-between">
+		<div class="flex flex-col lg:max-w-lg text-center lg:text-left">
+			<p class="text-4xl mb-1 font-bold">Usually using...</p>
+			<p class="text-small my-5 text-zinc-500">
+				These are the technologies I feel more comfortable using and have more experience with, but
+				I never reject the idea of trying new things!
+			</p>
+		</div>
+	</div>
+
+	<div>
+		<p class="text-sm font-semibold text-center lg:text-left my-2">Front-end</p>
+		<div class="flex flex-wrap lg:justify-start justify-center gap-2">
+			{#each technologies.frontEnd as tech}
+				<TechPill name={tech} />
+			{/each}
+		</div>
+		<p class="text-sm font-semibold text-center lg:text-left my-2">Back-end</p>
+		<div class="flex flex-wrap lg:justify-start justify-center gap-2">
+			{#each technologies.backEnd as tech}
+				<TechPill name={tech} />
+			{/each}
+		</div>
+		<p class="text-sm font-semibold text-center lg:text-left my-2">Others</p>
+		<div class="flex flex-wrap lg:justify-start justify-center gap-2">
+			{#each technologies.others as tech}
+				<TechPill name={tech} />
+			{/each}
+		</div>
+	</div>
+</section>
---
+++ src/components/technologies/TechPill.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<span
+	class="inline-flex items-center rounded-full bg-indigo-100 px-3 py-0.5 text-sm font-medium text-indigo-800"
+	>{name}</span
+>
