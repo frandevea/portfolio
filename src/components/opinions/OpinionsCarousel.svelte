b3e4f5971ff049c0380ae40ef3e6e30d53c4559b
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-05 00:10:25 +0200

    add pause on click and change directions of the marquee component

--- src/components/opinions/OpinionsCarousel.svelte
+++ src/components/opinions/OpinionsCarousel.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="bg-indigo-200 py-4">
-	<Marquee direction="right" speed={10}>
+	<Marquee direction="left" pauseOnClick={true} speed={10}>
 		{#each opinions as opinion}
 			<OpinionCard {opinion} />
 		{/each}
