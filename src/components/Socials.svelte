8e8700cb760845b5992558802d6f39d91c192934
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-04 22:23:06 +0200

    listed my accounts  on social media by a socials component

---
+++ src/components/Socials.svelte
@@ -0,0 +1,17 @@
+<script>
+	import GithubIcon from './icons/GithubIcon.svelte';
+	import LinkedInIcon from './icons/LinkedInIcon.svelte';
+	import TwitterIcon from './icons/TwitterIcon.svelte';
+</script>
+
+<div class="flex justify-center mb-10 text-indigo-500 gap-4 lg:justify-start">
+	<a href="https://www.linkedin.com/in/franperea/" target="_blank">
+		<LinkedInIcon props="w-8 " />
+	</a>
+	<a href="https://www.twitter.com/franpereadev/" target="_blank">
+		<TwitterIcon props="w-8" />
+	</a>
+	<a href="https://www.github.com/franpereadev/" target="_blank">
+		<GithubIcon props="w-8" />
+	</a>
+</div>
