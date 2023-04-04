31de16eeb145f89494c01f1a441d0d022bd141d2
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-04-04 22:22:29 +0200

    refactor the page header adding social links and better responsive

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,15 +1,19 @@
 <script>
 	import me from '$lib/images/opinions/me.jpg';
+	import Socials from './Socials.svelte';
 </script>
 
 <div class="py-16">
-	<div class="flex justify-around items-center flex-wrap">
-		<div class="pr-0 mb-10 lg:pr-5 lg:mb-0">
+	<div
+		class="flex items-center justify-center text-center flex-wrap sm:px-4 gap-x-20 lg:justify-between lg:text-left"
+	>
+		<div class="">
 			<p class="text-4xl mb-1 font-bold max-w-3xl">Hi! I am Fran Perea.</p>
-			<p class="text-2xl mb-3 font-bold max-w-xl">
+			<p class="text-2xl mb-4 font-bold max-w-xl">
 				Transforming <span class="text-indigo-500">ideas</span> into reality through
 				<span class="text-indigo-500">software</span> engineering is my calling.
 			</p>
+			<Socials />
 		</div>
 		<img src={me} alt="" class="rounded-full" />
 	</div>
