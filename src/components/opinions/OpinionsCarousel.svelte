b48041ab804e2936538a1a1815b6727447cce9a9
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-06-06 19:58:38 +0200

    feat: replace Opinions Carousel library

--- src/components/opinions/OpinionsCarousel.svelte
+++ src/components/opinions/OpinionsCarousel.svelte
@@ -1,13 +1,34 @@
 <script>
-	import Marquee from 'svelte-fast-marquee';
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
 	import OpinionCard from './OpinionCard.svelte';
 	import { opinions } from '$lib/data/opinions.data';
+	import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+	import '@splidejs/svelte-splide/css/core';
 </script>
 
-<div class="bg-indigo-200 py-4">
-	<Marquee direction="left" pauseOnClick={true} speed={10}>
-		{#each opinions as opinion}
+<Splide
+	aria-label="Opinion Cards"
+	class="bg-indigo-200 py-4 mt-2"
+	extensions={{ AutoScroll }}
+	options={{
+		type: 'loop',
+		drag: 'free',
+		focus: 'center',
+		perPage: 3,
+		autoScroll: {
+			speed: 0.2
+		},
+		rewind: false,
+		gap: '1rem',
+		arrows: false,
+		autoHeight: true,
+		autoWidth: true,
+		pauseOnHover: false
+	}}
+>
+	{#each opinions as opinion}
+		<SplideSlide>
 			<OpinionCard {opinion} />
-		{/each}
-	</Marquee>
-</div>
+		</SplideSlide>
+	{/each}
+</Splide>
